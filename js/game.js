feee784964293ec1957c8b0cec15383862cf7f4b
Anna Radford <awr36@drexel.edu>
2016-06-28 14:48:22 -0400

    Week 02 changes
    Starting to build solo pong game.

--- js/game.js
+++ js/game.js
@@ -19,4 +19,29 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+//console.log('Holla');
+//Step 01... Create game canvas and track mouse position
+
+var gameCanvas = document.getElementById("canvas");
+//Store HTML5 canvas tag into a JS variable
+
+var ctx = gameCanvas.getContext("2d");//Create context 2d
+var W = window.innerWidth;
+var H = window.innerHeight;
+var mouseObj = {};
+
+gameCanvas.width=W;
+gameCanvas.height=H;
+
+function paintCanvas(){
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, W, H);
+}
+paintCanvas();
+
+function trackPosition(evt){
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("cursor x is :" + mouseObj.x + "cursor y is :" + mouseObj.y)
+}
+gameCanvas.addEventListener("mousemove", trackPosition, true);
